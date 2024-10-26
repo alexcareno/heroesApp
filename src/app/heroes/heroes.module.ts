ef79af73e98cb10518d35e8c88cdb2f45c5de948
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2024-10-25 22:31:46 -0500

    Agregando angular material

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,19 +7,27 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    HeroPageComponent,
-    LayoutPageComponent,
-    ListPageComponent,
-    NewPageComponent,
-    SearchPageComponent
-  ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+	declarations: [
+		HeroPageComponent,
+		LayoutPageComponent,
+		ListPageComponent,
+		NewPageComponent,
+		SearchPageComponent,
+		CardComponent,
+		HeroImagePipe
+	],
+	imports: [
+		CommonModule,
+		HeroesRoutingModule,
+		MaterialModule,
+		ReactiveFormsModule
+	]
 })
 export class HeroesModule { }
