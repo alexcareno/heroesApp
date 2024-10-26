a252ab1c24d6f2a56f51a3c0cfb659e2a1500dae
Alejandro de Jesús Escareño Flores <alejandroef04@gmail.com>
2024-10-26 16:29:20 -0500

    Creando CRUD en servicio

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environtment';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
 
 	private baseUrl: string = environment.baseUrl;
@@ -24,5 +24,24 @@ export class HeroesService {
 
 	getSuggestions(query: string): Observable<Hero[]> {
 		return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+	}
+
+	addHero(hero: Hero): Observable<Hero> {
+		return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+	}
+
+	updateHero(hero: Hero): Observable<Hero> {
+		if (!hero.id) {
+			throw new Error('Hero id is required');
+		}
+		return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+	}
+
+	deleteHero(id: string): Observable<boolean> {
+		return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+			.pipe(
+				catchError(err => of(false)),
+				map(resp => true)
+			);
 	}
 }
